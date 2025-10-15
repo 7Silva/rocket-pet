edc1283ccd260ffc1806ed27e09d432e99e8f6c0
7Silva <zaronymogamer@gmail.com>
2025-10-15 11:04:03 -0300

    chore: create project

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,15 +10,27 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    'next/core-web-vitals',
+    'next/typescript',
+    'plugin:prettier/recommended'
+  ),
   {
     ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
+      'src/generated/prisma/**',
+      'pgdata/**',
+      '.next/**',
+      'node_modules/**',
+      'dist/**',
     ],
+  },
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    rules: {
+      'arrow-body-style': 'off',
+      'prefer-arrow-callback': 'off',
+      'prettier/prettier': 'error',
+    },
   },
 ];
 
