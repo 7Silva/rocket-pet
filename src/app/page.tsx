93c8bf3525ae64fa3e7f59087fa14b036cad8e9f
7Silva <zaronymogamer@gmail.com>
2025-10-15 11:12:33 -0300

    chore: remove unsed component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,7 @@
-type Props = {
-  title: string;
-};
-
-const Component = ({ title }: Props) => {
-  return (
-    <div>
-      <h1>Welcome to Rocket Pet!</h1>
-    </div>
-  );
-};
-
 export default function Home() {
   return (
     <div>
       <h1>Welcome to Rocket Pet!</h1>
-
-      <Component title={2} />
     </div>
   );
 }
